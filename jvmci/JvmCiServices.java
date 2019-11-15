92b951711868cb568c5e0e6894302a1a0ddd910b
Galder Zamarreño <galder@zamarreno.com>
2019-11-15 16:39:05 +0100

    Compile and run code that uses JVMCI

---
+++ jvmci/JvmCiServices.java
@@ -0,0 +1,10 @@
+// JdkVmCiServices.java
+import jdk.vm.ci.services.Services;
+
+public class JvmCiServices
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Services.getSavedProperties().get("java.specification.version"));
+    }
+}
