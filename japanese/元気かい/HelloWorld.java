2b9d287a3676da5fc74c1d0a952ed1de1757a131
Galder Zamarreño <galder@zamarreno.com>
2020-08-12 07:30:36 +0200

    Add Dockerfile forcing encoding to make example work

--- japanese/元気かい/HelloWorld.java
+++ japanese/元気かい/HelloWorld.java
@@ -5,10 +5,10 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        Path path = Paths.get("/home/g/tmp/japanese/元気かい/graalvm-20.1");
-        System.out.println("Path with japanese chars: " + path);
-
         Path userDir = Paths.get(System.getProperty("user.dir", "."));
         System.out.println("User dir: " + userDir);
+
+        Path path = Paths.get("/home/g/tmp/japanese/元気かい/graalvm-20.1");
+        System.out.println("Path with japanese chars: " + path);
     }
 }
