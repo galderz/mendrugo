eacad7add1b1f97dbd7208a54fe4b2d4de39c6ea
Galder Zamarreño <galder@zamarreno.com>
2020-08-17 11:03:05 +0200

    Update qollider script for latest changes

--- infinispan-native/qollider.sh
+++ infinispan-native/qollider.sh
@@ -6,26 +6,39 @@ pushd ~/1/qollider
 git pull
 
 ./qollider.sh \
- graal-build \
- --jdk-tree https://github.com/openjdk/jdk11u-dev/tree/master \
- --graal-tree https://github.com/graalvm/mandrel/tree/mandrel/20.1
+    jdk-build \
+    --tree https://github.com/openjdk/jdk11u-dev/tree/jdk-11.0.8%2B10
 
 ./qollider.sh \
- maven-build \
- --tree https://github.com/infinispan/infinispan/tree/11.0.x \
-  --additional-build-args -s,maven-settings.xml
-
-# Use snapshot Quarkus version that has includes third party to source cache
-./qollider.sh \
- maven-build \
- --tree https://github.com/galderz/quarkus/tree/t_debug_symbols_next
+    graal-build \
+    --tree https://github.com/graalvm/mandrel/tree/mandrel/20.1
 
 ./qollider.sh \
- maven-build \
- --tree https://github.com/galderz/infinispan-quarkus/tree/t_quarkus16_17 \
- --additional-build-args -pl,'!:infinispan-quarkus-integration-test-server',-Dquarkus.version=999-SNAPSHOT
+    maven-build \
+    --tree https://github.com/infinispan/infinispan/tree/master \
+    --additional-build-args \
+        -s \
+        maven-settings.xml
 
 ./qollider.sh \
- maven-build \
- --tree https://github.com/galderz/infinispan-quarkus/tree/t_quarkus16_17 \
- --additional-build-args -Dnative,-pl,:infinispan-quarkus-server-runner,-Dquarkus.version=999-SNAPSHOT,-Dquarkus.native.debug.enabled=true,dependency:sources
+    maven-build \
+    --tree https://github.com/quarkusio/quarkus/tree/master
+
+./qollider.sh \
+    maven-build \
+    --tree https://github.com/infinispan/infinispan-quarkus/tree/master \
+    --additional-build-args \
+        -pl \
+        '!:infinispan-quarkus-integration-test-server' \
+        -Dquarkus.version=999-SNAPSHOT
+
+./qollider.sh \
+    maven-build \
+    --tree https://github.com/infinispan/infinispan-quarkus/tree/master \
+    --additional-build-args \
+        -Dnative \
+        -pl \
+        :infinispan-quarkus-server-runner \
+        -Dquarkus.version=999-SNAPSHOT \
+        -Dquarkus.native.debug.enabled=true \
+        dependency:sources
