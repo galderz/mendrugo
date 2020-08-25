df9a0c9994058c2888e985af0c6c6220d129efc1
Galder Zamarreño <galder@zamarreno.com>
2020-08-25 07:54:49 +0200

    Fix script for new name and location

--- infinispan-native/qollider-20.1.sh
+++ infinispan-native/qollider-20.1.sh
@@ -7,26 +7,26 @@ pushd ~/1/qollider
 
 git pull
 
-./qollider.sh \
+qollider \
     jdk-get \
     --url https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u265-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u265b01.tar.gz
 
-./qollider.sh \
+qollider \
     graal-build \
     --tree https://github.com/graalvm/mandrel/tree/mandrel/20.1
 
-./qollider.sh \
+qollider \
     maven-build \
     --tree https://github.com/infinispan/infinispan/tree/11.0.x \
     --additional-build-args \
         -s \
         maven-settings.xml
 
-./qollider.sh \
+qollider \
     maven-build \
     --tree https://github.com/quarkusio/quarkus/tree/master
 
-./qollider.sh \
+qollider \
     maven-build \
     --tree ${ISPN_QUARKUS} \
     --additional-build-args \
@@ -34,7 +34,7 @@ git pull
         '!:infinispan-quarkus-integration-test-server' \
         -Dquarkus.version=999-SNAPSHOT
 
-./qollider.sh \
+qollider \
     maven-build \
     --tree ${ISPN_QUARKUS} \
     --additional-build-args \
