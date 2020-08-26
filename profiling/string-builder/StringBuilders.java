d00c3195cdf651e659fb9c4963764f64d9dbaf8b
Galder Zamarreño <galder@zamarreno.com>
2020-08-26 15:27:09 +0200

    Minor changes

--- profiling/string-builder/StringBuilders.java
+++ profiling/string-builder/StringBuilders.java
@@ -1,4 +1,15 @@
 // https://github.com/apangin/codeone2019-java-profiling/blob/master/src/demo1/StringBuilderTest.java
+
+/**
+ * This demo shows that most sampling profilers are misleading.
+ * The given program appends 5 symbols to the end of StringBuilder and deletes 5 symbols from the beginning of StringBuilder.
+ * <p>
+ * The real bottleneck here is delete(),
+ * since it involves moving of 1 million characters.
+ * However, safepoint-based profilers will display Thread.isAlive() as the hottest method.
+ * JFR will not port anything useful at all,
+ * since it cannot traverse stack traces when JVM is running System.arraycopy().
+ */
 public class StringBuilders
 {
     public static void main(String[] args)
@@ -6,7 +17,8 @@ public class StringBuilders
         StringBuilder sb = new StringBuilder();
         sb.append(new char[1_000_000]);
 
-        do {
+        do
+        {
             sb.append(12345);
             sb.delete(0, 5);
         } while (Thread.currentThread().isAlive());
