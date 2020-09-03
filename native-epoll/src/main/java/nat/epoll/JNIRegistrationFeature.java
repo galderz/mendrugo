94a4538970cb294a56c12544d20c8c43f3db351a
Galder Zamarreño <galder@zamarreno.com>
2020-09-03 07:36:37 +0200

    Adjust reflection lookups for JNI registration

--- native-epoll/src/main/java/nat/epoll/JNIRegistrationFeature.java
+++ native-epoll/src/main/java/nat/epoll/JNIRegistrationFeature.java
@@ -9,51 +9,11 @@ public class JNIRegistrationFeature implements Feature
 {
     public void beforeAnalysis(BeforeAnalysisAccess access)
     {
-        try
-        {
-            defaultFileRegion();
-            nativeDatagramPacket();
-            peerCredentials();
-            datagramSocket();
-            JNIRuntimeAccess.register(Class.forName("io.netty.channel.ChannelException"));
-        }
-        catch (ClassNotFoundException | NoSuchFieldException | NoSuchMethodException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void defaultFileRegion() throws ClassNotFoundException, NoSuchFieldException
-    {
-        final Class<?> clazz = Class.forName("io.netty.channel.DefaultFileRegion");
-        JNIRuntimeAccess.register(clazz);
-        JNIRuntimeAccess.register(clazz.getField("file"));
-        JNIRuntimeAccess.register(clazz.getField("transferred"));
-    }
-
-    private void nativeDatagramPacket() throws ClassNotFoundException, NoSuchFieldException
-    {
-        final Class<?> clazz = Class.forName("io.netty.channel.epoll.NativeDatagramPacketArray$NativeDatagramPacket");
-        JNIRuntimeAccess.register(clazz);
-        JNIRuntimeAccess.register(clazz.getField("addr"));
-        JNIRuntimeAccess.register(clazz.getField("addrLen"));
-        JNIRuntimeAccess.register(clazz.getField("scopeId"));
-        JNIRuntimeAccess.register(clazz.getField("port"));
-        JNIRuntimeAccess.register(clazz.getField("memoryAddress"));
-        JNIRuntimeAccess.register(clazz.getField("count"));
-    }
-
-    private void peerCredentials() throws ClassNotFoundException, NoSuchMethodException
-    {
-        final Class<?> clazz = Class.forName("io.netty.channel.unix.PeerCredentials");
-        JNIRuntimeAccess.register(clazz);
-        JNIRuntimeAccess.register(clazz.getMethod("<init>"));
-    }
-
-    private void datagramSocket() throws ClassNotFoundException, NoSuchMethodException
-    {
-        final Class<?> clazz = Class.forName("io.netty.channel.unix.DatagramSocketAddress");
-        JNIRuntimeAccess.register(clazz);
-        JNIRuntimeAccess.register(clazz.getMethod("<init>"));
+        final var effects = new NettyJNI.Effects(
+            JNIRuntimeAccess::register
+            , JNIRuntimeAccess::register
+            , JNIRuntimeAccess::register
+        );
+        NettyJNI.registerJNI(effects);
     }
 }
