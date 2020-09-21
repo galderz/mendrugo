29748380c4b5a7c1378233d2f8572a9e834dbb69
Galder Zamarreño <galder@redhat.com>
2020-09-21 18:59:53 +0200

    Use public native function

--- epoll-c/src/main/java/epoll/c/graal/c/NettyUnixLimits.java
+++ epoll-c/src/main/java/epoll/c/graal/c/NettyUnixLimits.java
@@ -7,6 +7,6 @@ import org.graalvm.word.WordBase;
 @CLibrary(value = "netty_transport_native_epoll_x86_64")
 public class NettyUnixLimits
 {
-    @CFunction(value = "netty_unix_limits_udsSunPathSize", transition = CFunction.Transition.NO_TRANSITION)
+    @CFunction(value = "io_netty_unix_limits_udsSunPathSize", transition = CFunction.Transition.NO_TRANSITION)
     public static native int udsSunPathSize(WordBase jnienv, WordBase clazz);
 }
