3fa5b0cc7eff1caf7e3d1fd6ed7fcc56b772d883
Galder Zamarreño <galder@redhat.com>
2020-09-21 10:59:36 +0200

    Minor adjustments to epoll C example

--- epoll-c/src/main/java/epoll/c/graal/c/NettyUnixLimits.java
+++ epoll-c/src/main/java/epoll/c/graal/c/NettyUnixLimits.java
@@ -4,11 +4,9 @@ import org.graalvm.nativeimage.c.function.CFunction;
 import org.graalvm.nativeimage.c.function.CLibrary;
 import org.graalvm.word.WordBase;
 
-// TODO it's an so, so it's dynamic
-// TODO try to remove lib prefix
-@CLibrary(value = "libnetty_transport_native_epoll_x86_64", requireStatic = true)
+@CLibrary(value = "netty_transport_native_epoll_x86_64")
 public class NettyUnixLimits
 {
-    @CFunction(value = "Java_io_netty_channel_unix_LimitsStaticallyReferencedJniMethods_udsSunPathSize", transition = CFunction.Transition.NO_TRANSITION)
+    @CFunction(value = "netty_unix_limits_udsSunPathSize", transition = CFunction.Transition.NO_TRANSITION)
     public static native int udsSunPathSize(WordBase jnienv, WordBase clazz);
 }
