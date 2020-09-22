0e36f92c556f51623f134578dbe39c23a198defb
Galder Zamarreño <galder@redhat.com>
2020-09-22 13:46:10 +0200

    Use static Netty libraries instead of dynamic

--- epoll-c/src/main/java/epoll/c/graal/c/NettyUnixLimits.java
+++ epoll-c/src/main/java/epoll/c/graal/c/NettyUnixLimits.java
@@ -4,7 +4,7 @@ import org.graalvm.nativeimage.c.function.CFunction;
 import org.graalvm.nativeimage.c.function.CLibrary;
 import org.graalvm.word.WordBase;
 
-@CLibrary(value = "netty_transport_native_epoll_x86_64")
+@CLibrary(value = "netty-unix-common", requireStatic = true)
 public class NettyUnixLimits
 {
     @CFunction(value = "io_netty_unix_limits_udsSunPathSize", transition = CFunction.Transition.NO_TRANSITION)
