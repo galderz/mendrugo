f3f94f1078e42d303a04b69ae1d991e0d2e2ebeb
Galder Zamarreño <galder@redhat.com>
2020-09-25 16:44:58 +0200

    Client also should use epoll socket channel

--- epoll-c/src/main/java/epoll/c/example/EpollClient.java
+++ epoll-c/src/main/java/epoll/c/example/EpollClient.java
@@ -9,9 +9,9 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollSocketChannel;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CountDownLatch;
@@ -23,14 +23,14 @@ public class EpollClient implements AutoCloseable
     static final String HOST = System.getProperty("host", "127.0.0.1");
     static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
 
-    final EventLoopGroup group = new NioEventLoopGroup();
+    final EventLoopGroup group = new EpollEventLoopGroup();
     final CountDownLatch echoLatch = new CountDownLatch(1);
 
     public void run() throws InterruptedException
     {
         Bootstrap b = new Bootstrap();
         b.group(group)
-            .channel(NioSocketChannel.class)
+            .channel(EpollSocketChannel.class)
             .option(ChannelOption.TCP_NODELAY, true)
             .handler(new ChannelInitializer<SocketChannel>()
             {
