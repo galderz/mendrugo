a2e30239e15a40cb931ac68e9f241c1d428d102e
Galder Zamarreño <galder@redhat.com>
2020-10-15 10:55:44 +0200

    Trace is enabled returned true when computed statically

--- quarkus-logging/src/main/java/org/acme/getting/started/commandmode/GreetingMain.java
+++ quarkus-logging/src/main/java/org/acme/getting/started/commandmode/GreetingMain.java
@@ -4,15 +4,26 @@ import javax.inject.Inject;
 
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import org.jboss.logging.Logger;
 
 @QuarkusMain
 public class GreetingMain implements QuarkusApplication {
+
+    private static final Logger LOG = Logger.getLogger(GreetingMain.class);
+    private static final boolean isTrace = LOG.isTraceEnabled();
 
     @Inject
     GreetingService service;
 
     @Override
     public int run(String... args) {
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("Compute whether trace enabled on the fly");
+        }
+
+        if (isTrace) {
+            LOG.trace(getMessage());
+        }
 
         if(args.length>0) {
             System.out.println(service.greeting(String.join(" ", args)));
@@ -23,5 +34,9 @@ public class GreetingMain implements QuarkusApplication {
         return 0;
     }
 
+    private String getMessage() {
+        System.out.println("Working very hard to provide a trace message");
+        return "Use cached value to see if trace enabled";
+    }
 
 }
