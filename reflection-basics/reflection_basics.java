46c925694b1344b0733b17cf65e980a604f29a51
Galder Zamarreño <galder@redhat.com>
2020-12-09 15:08:40 +0100

    Trying different ways to test reflection method inspection

--- reflection-basics/reflection_basics.java
+++ reflection-basics/reflection_basics.java
@@ -1,4 +1,10 @@
+import java.lang.reflect.Method;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
+import java.util.function.UnaryOperator;
+
+import static java.security.AccessController.doPrivileged;
 
 class reflection_basics
 {
@@ -7,7 +13,76 @@ class reflection_basics
         needEnabledAsserts();
         invokeReflectMethodViaBakedClass();
         invokeReflectMethodViaParamClass(ArrayList.class);
+        invokePrivilegedReflectMethodViaParamClass(ArrayList.class);
+        lazyInvokePrivilegedReflectMethodViaParamClass(ArrayList.class, null);
         System.out.println("Assertions passed");
+    }
+
+    private static void lazyInvokePrivilegedReflectMethodViaParamClass(final Class<?> clazz, final Class<?> returnType) throws Exception
+    {
+        UnaryOperator<Object> lazy = lazyReflectClone(clazz);
+
+        final var list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        assert list.equals(list.clone());
+        assert list.equals(lazy.apply(list));
+    }
+
+    private static UnaryOperator<Object> lazyReflectClone(Class<?> clazz)
+    {
+        final var method = doPrivileged((PrivilegedAction<Method>) () ->
+        {
+            try
+            {
+                return clazz.getMethod("clone");
+            }
+            catch (NoSuchMethodException e)
+            {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return obj ->
+        {
+            try
+            {
+                return method.invoke(obj);
+            }
+            catch (RuntimeException | Error e)
+            {
+                throw e;
+            }
+            catch (Throwable throwable)
+            {
+                throw new UndeclaredThrowableException(throwable);
+            }
+        };
+    }
+
+    private static void invokePrivilegedReflectMethodViaParamClass(Class<?> clazz) throws Exception
+    {
+        final var method = doPrivileged((PrivilegedAction<Method>) () ->
+        {
+            try
+            {
+                return clazz.getMethod("clone");
+            }
+            catch (NoSuchMethodException e)
+            {
+                throw new RuntimeException(e);
+            }
+        });
+
+        final var list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        assert list.equals(list.clone());
+        assert list.equals(method.invoke(list));
     }
 
     private static void invokeReflectMethodViaParamClass(Class<?> clazz) throws Exception
