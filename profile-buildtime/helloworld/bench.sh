e6250ac5fee5fdf357db3570f91fe5ba0de0c7d8
Galder Zamarreño <galder@redhat.com>
2021-07-13 14:20:19 +0200

    Bench with different arguments

--- profile-buildtime/helloworld/bench.sh
+++ profile-buildtime/helloworld/bench.sh
@@ -2,12 +2,34 @@
 
 set -e
 
-for name in graalvm-20.3-dev graalvm-21.2-dev
-do
-    NAME=$name make clean
-    NAME=$name make
+argsBench()
+{
+    local name=$1
+    local args=$2
+    NAME=$name EXTRA_ARGS=$args make clean
+    NAME=$name EXTRA_ARGS=$args make
     for i in `seq 1 10`
     do
-        NAME=$name make | tee -a "target/$name/console.log"
+        NAME=$name EXTRA_ARGS=$args make | tee -a "target/$name/console.log"
     done
+}
+
+argsBench "parse_once" "-H:-ParseOnce"
+argsBench "locale_opt" "-H:+LocalizationOptimizedMode"
+argsBench "locale_opt-parse_once" "-H:+LocalizationOptimizedMode,-H:-ParseOnce"
+
+homeBench()
+{
+    local graalvm_home=$1
+    GRAALVM_HOME=$graalvm_home make clean
+    GRAALVM_HOME=$graalvm_home make
+    for i in `seq 1 10`
+    do
+        GRAALVM_HOME=$graalvm_home make | tee -a "target/$graalvm_home/console.log"
+    done
+}
+
+for graalvm_home in graalvm-20.3-dev graalvm-21.2-dev
+do
+    homeBench $graalvm_home
 done
