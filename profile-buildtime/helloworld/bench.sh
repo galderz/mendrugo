6268203aab4db0d5f2631625b1992154610ea800
Galder Zamarreño <galder@redhat.com>
2021-07-13 16:07:58 +0200

    Add reports output to console output

--- profile-buildtime/helloworld/bench.sh
+++ profile-buildtime/helloworld/bench.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+make clean-all
+
 argsBench()
 {
     local name=$1
     local args=$2
     NAME=$name EXTRA_ARGS=$args make clean
-    NAME=$name EXTRA_ARGS=$args make
+    NAME=$name EXTRA_ARGS=$args make reports | tee -a "target/$name/console.log"
     for i in `seq 1 10`
     do
         NAME=$name EXTRA_ARGS=$args make | tee -a "target/$name/console.log"
@@ -22,7 +24,7 @@ homeBench()
 {
     local graalvm_home=$1
     GRAALVM_HOME=$graalvm_home make clean
-    GRAALVM_HOME=$graalvm_home make
+    GRAALVM_HOME=$graalvm_home make reports | tee -a "target/$graalvm_home/console.log"
     for i in `seq 1 10`
     do
         GRAALVM_HOME=$graalvm_home make | tee -a "target/$graalvm_home/console.log"
