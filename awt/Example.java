7b6dc225471735e5bba4b2137f11791319deaa8d
Galder Zamarreño <galder@redhat.com>
2021-09-13 14:59:07 +0200

    Try to get AWT toolkit

--- awt/Example.java
+++ awt/Example.java
@@ -1,9 +1,11 @@
 import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
 
 public class Example
 {
     public static void main(String[] args) throws Exception
     {
         System.out.println("AWT Graphics, is headless? " + GraphicsEnvironment.isHeadless());
+        System.out.println("AWT Toolkit: " + Toolkit.getDefaultToolkit());
     }
 }
