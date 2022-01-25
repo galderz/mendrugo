c545dfb8a6ca57c513198a1bae240325c14c55e9
Galder Zamarreño <galder@zamarreno.com>
2022-01-25 10:30:15 +0100

    Optional dependency example

--- maven-optional/main-project/src/main/java/org/example/main/graal/Target_org_example_optional_NumberParser.java
+++
@@ -1,8 +0,0 @@
-package org.example.main.graal;
-
-import com.oracle.svm.core.annotate.TargetClass;
-
-@TargetClass(className = "org.example.optional.NumberParser")
-final class Target_org_example_optional_NumberParser
-{
-}
--- maven-optional/main-project/src/main/java/org/example/main/graal/Target_org_example_project_Parser.java
+++ maven-optional/main-project/src/main/java/org/example/main/graal/Target_org_example_project_Parser.java
@@ -2,11 +2,12 @@ package org.example.main.graal;
 
 import com.oracle.svm.core.annotate.Delete;
 import com.oracle.svm.core.annotate.TargetClass;
+import org.example.optional.NumberParser;
 
 @TargetClass(className = "org.example.project.Parser")
 final class Target_org_example_project_Parser
 {
     @Delete
-    Target_org_example_optional_NumberParser numberParser;
+    NumberParser numberParser;
 }
 
