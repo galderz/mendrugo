ca4f6ff83701bd1b36f85765ca73b324424e1e1d
Galder Zamarreño <galder@redhat.com>
2022-01-31 17:54:45 +0100

    Play around with a problematic toString() which does not get called

---
+++ tostring/src/main/java/org/example/tostring/Main.java
@@ -0,0 +1,10 @@
+package org.example.tostring;
+
+public class Main
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        // System.out.println("Hello World!");
+        new Parsing().run();
+    }
+}
