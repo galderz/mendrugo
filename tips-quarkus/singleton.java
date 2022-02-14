85e7bbed3dc909fdc052b3a6eba891ea928f0755
Galder Zamarreño <galder@redhat.com>
2022-02-14 16:47:11 +0100

    Singletons with static or Enum are baked with Quarkus

---
+++ tips-quarkus/singleton.java
@@ -0,0 +1,69 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.quarkus:quarkus-resteasy:2.7.1.Final
+//JAVAC_OPTIONS -parameters
+//JAVA_OPTIONS -Djava.util.logging.manager=org.jboss.logmanager.LogManager
+//Q:CONFIG quarkus.native.enable-reports=true
+//Q:CONFIG quarkus.native.additional-build-args=--initialize-at-run-time=Time
+
+import io.quarkus.runtime.Quarkus;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+@Path("/singleton/start-time")
+@ApplicationScoped
+public class singleton
+{
+    @GET
+    @Path("/static")
+    public long withStatic()
+    {
+	return StaticSingleton.startTime();
+    }
+ 
+    @GET
+    @Path("/enum")
+    public long withEnum()
+    {
+	return EnumSingleton.INSTANCE.startTime();
+    }
+   
+    public static void main(String[] args)
+    {
+        Quarkus.run(args);
+    }
+}
+
+enum EnumSingleton
+{
+    INSTANCE(System.currentTimeMillis());
+
+    private final long startTime;
+
+    private EnumSingleton(long startTime)
+    {
+	this.startTime = startTime;
+    }
+
+    long startTime()
+    {
+	return startTime;
+    }
+}
+
+class StaticSingleton
+{
+    static final long START_TIME = System.currentTimeMillis();
+
+    static long startTime()
+    {
+	return START_TIME;
+    }
+}
