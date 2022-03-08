cc2d5092bd157c9f60f27ef2dcb1cb1b270a94fa
Galder Zamarreño <galder@zamarreno.com>
2022-03-08 11:38:50 +0100

    Update analyse script to match expectations

--- profile-buildtime/analyse.java
+++ profile-buildtime/analyse.java
@@ -4,7 +4,6 @@
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Parameters;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,16 +16,12 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -35,9 +30,6 @@ import java.util.stream.Stream;
     description = "analyse made with jbang")
 class analyse implements Callable<Integer>
 {
-    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
-    private String greeting;
-
     public static void main(String... args)
     {
         int exitCode = new CommandLine(new analyse()).execute(args);
@@ -60,7 +52,7 @@ class analyse implements Callable<Integer>
             .collect(Collectors.toList());
 
         writeCsvFile(toTotalCsv(jobResults, tr -> tr.duration.toString()));
-        writeCsvFile(toTotalCsv(jobResults, tr -> String.valueOf(tr.maxRSS)));
+        writeCsvFile(toTotalCsv(jobResults, tr -> String.valueOf(tr.maxRss)));
         writeCsvFile(toUsedReports(jobResults));
         return 0;
     }
@@ -140,72 +132,6 @@ class analyse implements Callable<Integer>
         return new Csv("native-image-totals", result);
     }
 
-//    Csv toPhaseCsv(JobResult job)
-//    {
-//        final int numPhases = 8;
-//        final AtomicInteger counter = new AtomicInteger();
-//
-//        final List<Phase> affectsMemory = List.of(Phase.CLASSLIST, Phase.ANALYSIS, Phase.COMPILE, Phase.IMAGE);
-//
-//        final List<String> values = job.phases.stream()
-//            .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / numPhases))
-//            .entrySet()
-//            .stream()
-//            .map(phaseRun ->
-//                String.format(
-//                    "%s,%s,%s"
-//                    , toPhaseTimeCsv(job.name, phaseRun.getKey(), phaseRun.getValue())
-//                    , toPhaseMemoryCsv(job.name, phaseRun.getKey(), phaseRun.getValue(), Phase::notTotal)
-//                    , toPhaseMemoryCsv(job.name, phaseRun.getKey(), phaseRun.getValue(), p -> p.contains(affectsMemory))
-//                )
-//            )
-//            .collect(Collectors.toList());
-//
-//        final String header = job.phases.stream()
-//            .limit(numPhases - 1)
-//            .map(p -> p.phase.toTitle())
-//            .collect(Collectors.joining(",", ",", ",Name"));
-//
-//        final String affectsMemoryHeader = affectsMemory.stream()
-//            .map(Phase::toTitle)
-//            .collect(Collectors.joining(",", ",", ",Name"));
-//
-//        List<String> result = new ArrayList<>();
-//        result.add(String.format("%s,%s,%s", header, header, affectsMemoryHeader));
-//        result.addAll(values);
-//        return new Csv("native-image-phases", result);
-//    }
-
-//    private String toPhaseTimeCsv(String name, int run, List<PhaseResult> phases)
-//    {
-//        return phases.stream()
-//            .filter(p -> p.phase.notTotal())
-//            .map(p -> p.duration.toMillis())
-//            .map(String::valueOf)
-//            .collect(
-//                Collectors.joining(
-//                    ","
-//                    , String.format("Run %s,", run + 1)
-//                    , String.format(",%s", name)
-//                )
-//            );
-//    }
-
-//    private String toPhaseMemoryCsv(String name, int run, List<PhaseResult> phases, Predicate<Phase> phaseFilter)
-//    {
-//        return phases.stream()
-//            .filter(p -> phaseFilter.test(p.phase))
-//            .map(PhaseResult::memory)
-//            .map(String::valueOf)
-//            .collect(
-//                Collectors.joining(
-//                    ","
-//                    , String.format("Run %s,", run + 1)
-//                    , String.format(",%s", name)
-//                )
-//            );
-//    }
-
     JobResult toJobResult(File job)
     {
         final Path jobPath = job.toPath();
@@ -214,7 +140,7 @@ class analyse implements Callable<Integer>
         final long usedClasses = countLines("used_classes", reportsPath);
         final long usedMethods = countLines("used_methods", reportsPath);
         final long usedPackages = countLines("used_packages", reportsPath);
-        try(Stream<String> lines = Files.lines(jobPath.resolve("console.log")))
+        try(Stream<String> lines = Files.lines(jobPath.resolve("times.log")))
         {
             final List<TrialResult> trialResults = toTrialResults(lines);
             return new JobResult(jobName, usedClasses, usedMethods, usedPackages, trialResults);
@@ -257,27 +183,39 @@ class analyse implements Callable<Integer>
 
     private List<TrialResult> toTrialResults(Stream<String> lines)
     {
-        int groupBy = 2;
+        int groupBy = 3;
         AtomicInteger index = new AtomicInteger(0);
 
         final Map<Integer, List<String>> rawResults = lines
-            .filter(line -> line.endsWith("wall clock") || line.contains("Maximum resident set size"))
-            .collect(Collectors.groupingBy(ignore -> index.getAndIncrement() / groupBy));
+            .filter(line ->
+                line.contains("Command being timed")
+                    || line.contains("wall clock")
+                    || line.contains("Maximum resident set size")
+            ).collect(Collectors.groupingBy(ignore -> index.getAndIncrement() / groupBy));
 
         return rawResults.values().stream()
             .map(this::toTrialResult)
+            .filter(tr -> !tr.duration.equals(Duration.ZERO))
             .toList();
     }
 
     TrialResult toTrialResult(List<String> values)
     {
-        final LocalTime time = LocalTime.parse(lastOf(values.get(0)), DateTimeFormatter.ofPattern("m:ss"));
-        final Duration wallClock = Duration.between(LocalTime.MIN, time);
+        final String commandLine = values.get(0);
+        if (commandLine.contains("runner.jar"))
+        {
+            final String wallClockLine = values.get(1);
+            final LocalTime time = LocalTime.parse(lastOf(wallClockLine), DateTimeFormatter.ofPattern("m:ss"));
+            final Duration wallClock = Duration.between(LocalTime.MIN, time);
 
-        final String rssKbytes = lastOf(values.get(1));
-        final double maxRSS = Double.parseDouble(rssKbytes) / 1024 / 1024;
+            final String maxRssLine = values.get(2);
+            final String maxRssKbytes = lastOf(maxRssLine);
+            final double maxRss = Double.parseDouble(maxRssKbytes) / 1024 / 1024;
 
-        return new TrialResult(wallClock, maxRSS);
+            return new TrialResult(wallClock, maxRss);
+        }
+
+        return new TrialResult(Duration.ZERO, -1);
     }
 
     String lastOf(String line)
@@ -285,19 +223,6 @@ class analyse implements Callable<Integer>
         final String[] elems = line.split("\\s+");
         return elems[elems.length - 1];
     }
-
-//    String[] toPhaseResult(String line)
-//    {
-//        final String[] elements = line.split("\\s+");
-//        final String phase = elements[1].replaceAll("[\\[\\]:]", "");
-//        final String duration = elements[2].replace(",", "");
-//        final String memory = elements[4];
-//        return new PhaseResult(
-//            Phase.valueOf(phase.toUpperCase(Locale.ROOT))
-//            , Duration.ofMillis(Math.round(Double.parseDouble(duration)))
-//            , Double.parseDouble(memory)
-//        );
-//    }
 
     record JobResult(
         String name
@@ -311,7 +236,7 @@ class analyse implements Callable<Integer>
      * Duration in milliseconds.
      * Max RSS in GB
      */
-    record TrialResult(Duration duration, double maxRSS) {}
+    record TrialResult(Duration duration, double maxRss) {}
 
     record Csv(String name, Iterable<String> lines) {}
 }
