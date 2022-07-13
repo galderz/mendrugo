e482687c5067cabbf3ae9846d98dad5ee8f8b2ff
Galder Zamarreño <galder@redhat.com>
2022-07-13 16:56:45 +0200

    Compute bundle and config locations per thread

--- profile-buildtime/hibernate-orm-byteman/src/main/java/org/acme/byteman/ReflectionRegistryHelper.java
+++ profile-buildtime/hibernate-orm-byteman/src/main/java/org/acme/byteman/ReflectionRegistryHelper.java
@@ -7,10 +7,12 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -18,8 +20,8 @@ public class ReflectionRegistryHelper extends Helper
 {
     static final Map<Class<?>, Reason> REFLECTION_REASONS = new ConcurrentHashMap<>();
 
-    static Object CONFIGURATION_LOCATION;
-    static String BUNDLE_NAME;
+    static final Map<Thread, Object> CONFIGURATION_LOCATIONS = new ConcurrentHashMap<>();
+    static final Map<Thread, Object> BUNDLE_NAMES = new ConcurrentHashMap<>();
 
     protected ReflectionRegistryHelper(Rule rule)
     {
@@ -28,12 +30,12 @@ public class ReflectionRegistryHelper extends Helper
 
     public void trackConfigurationParsing(Object location)
     {
-        CONFIGURATION_LOCATION = location;
+        CONFIGURATION_LOCATIONS.put(Thread.currentThread(), location);
     }
 
     public void trackBundle(String bundleName)
     {
-        BUNDLE_NAME = bundleName;
+        BUNDLE_NAMES.put(Thread.currentThread(), bundleName);
     }
 
     public void trackReflectionRegistration(int numIteration, Object obj)
@@ -46,44 +48,43 @@ public class ReflectionRegistryHelper extends Helper
         final String stack = formatStack();
         if (stack.contains("org.hibernate.graalvm.internal.GraalVMStaticAutofeature"))
         {
-            REFLECTION_REASONS.put(clazz, new Reason(numIteration, clazz
+            REFLECTION_REASONS.putIfAbsent(clazz, new Reason(numIteration, clazz
                 , "Because Hibernate registered statically"
             ));
         }
         else if (stack.contains("com.oracle.svm.hosted.config.ConfigurationParserUtils"))
         {
-            REFLECTION_REASONS.put(clazz, new Reason(numIteration, clazz
-                , "Because it was defined in configuration file %s".formatted(CONFIGURATION_LOCATION.toString())
+            REFLECTION_REASONS.putIfAbsent(clazz, new Reason(numIteration, clazz
+                , "Because it was defined in configuration file %s".formatted(CONFIGURATION_LOCATIONS.get(Thread.currentThread()))
             ));
         }
         else if (stack.contains("com.oracle.svm.core.jdk.localization.LocalizationSupport"))
         {
-            REFLECTION_REASONS.put(clazz, new Reason(numIteration, clazz
-                , "Because of bundle %s".formatted(BUNDLE_NAME)
+            REFLECTION_REASONS.putIfAbsent(clazz, new Reason(numIteration, clazz
+                , "Because of bundle %s".formatted(BUNDLE_NAMES.get(Thread.currentThread()))
             ));
         }
         else if (stack.contains("io.quarkus.runner.AutoFeature"))
         {
-            REFLECTION_REASONS.put(clazz, new Reason(numIteration, clazz
+            REFLECTION_REASONS.putIfAbsent(clazz, new Reason(numIteration, clazz
                 , "Because Quarkus registers it directly or via an extension"
             ));
         }
         else if (stack.contains("com.oracle.svm.hosted.jdk.JNIRegistrationJavaNio"))
         {
-            REFLECTION_REASONS.put(clazz, new Reason(numIteration, clazz
+            REFLECTION_REASONS.putIfAbsent(clazz, new Reason(numIteration, clazz
                 , "Because GraalVM registers it for JNI access to Java NIO"
             ));
         }
         else if (stack.contains("com.oracle.svm.reflect.hosted.ReflectionFeature.beforeAnalysis"))
         {
-            REFLECTION_REASONS.put(clazz, new Reason(numIteration, clazz
+            REFLECTION_REASONS.putIfAbsent(clazz, new Reason(numIteration, clazz
                 , "Because GraalVM registers it for Object[]"
             ));
         }
         else
         {
-            System.out.printf("Unknown reflection registration of %s at %d iteration:%n", clazz, numIteration);
-            System.out.println(formatStack());
+            System.out.printf("Unknown reflection registration of %s at %d iteration:%n%s%n", clazz, numIteration, formatStack());
         }
     }
 
