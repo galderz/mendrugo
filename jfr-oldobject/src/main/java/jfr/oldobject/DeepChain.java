215b8b13aa23b8d57e8c427c685fec2f11d4c504
Galder Zamarreño <galder@zamarreno.com>
2022-10-31 08:55:08 +0100

    Add deep chain example

---
+++ jfr-oldobject/src/main/java/jfr/oldobject/DeepChain.java
@@ -0,0 +1,38 @@
+package jfr.oldobject;
+
+import java.util.Scanner;
+
+public class DeepChain
+{
+    static Object leak;
+
+    public static void main(String[] args)
+    {
+        Node node = new Node();
+        leak = node;
+        for (int i = 0; i < 1_000_000; i++) {
+            node.value = new Big();
+            node.left = new Node();
+            node.right = new Node();
+            node = node.right;
+        }
+
+        System.out.println("Press enter to finish program");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+
+    static class Node {
+        Node left;
+        Node right;
+        Object value;
+    }
+
+    static class Big {
+        public long value1;
+        public Object value2;
+        float value3;
+        int value4;
+        double value5;
+    }
+}
