bd2962b63068d60e2dfc570486c49f0f101ad0d2
Galder Zamarreño <galder@zamarreno.com>
2022-11-01 08:59:04 +0100

    Make sure leak object is used before end of method

--- jfr-oldobject/src/main/java/jfr/oldobject/DeepChain.java
+++ jfr-oldobject/src/main/java/jfr/oldobject/DeepChain.java
@@ -20,6 +20,12 @@ public class DeepChain
         System.out.println("Press enter to finish program");
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
+
+        // Use the leak, to avoid being dead code eliminated
+        if (leak.hashCode() == System.nanoTime())
+        {
+            System.out.println(" ");
+        }
     }
 
     static class Node {
