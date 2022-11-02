b8a1bbc4bd359f8d52acc5edcba772e46b0ca3c5
Galder Zamarreño <galder@zamarreno.com>
2022-11-02 09:21:45 +0100

    Show values for various properties at runtime

--- show-properties/src/main/java/org/acme/GreetingResource.java
+++ show-properties/src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,10 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        System.out.println("java.runtime.version=" + System.getProperty("java.runtime.version"));
+        System.out.println("java.vm.vendor=" + System.getProperty("java.vm.vendor"));
+        System.out.println("java.runtime.name=" + System.getProperty("java.runtime.name"));
+
         return "Hello from RESTEasy Reactive";
     }
 }
