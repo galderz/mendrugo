938fd61b653cc06b1684b1e0b7aa321051ad52c9
Galder Zamarreño <galder@redhat.com>
2022-12-07 12:23:14 +0100

    Automate stack counting via a gdb script

---
+++ start-stop/stackcount.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+stackcount()
+{
+    local stacksdir=$1
+    local name=$2
+    local function=$3
+    local duration=$4
+
+    mkdir -p $stacksdir
+    rm -f $stacksdir/$name.stacks
+    sudo /usr/share/bcc/tools/stackcount -D $duration -P -p $(pidof quarkus-runner) -U $function > $stacksdir/$name.stacks
+}
+
+stackcount target/stacks $1 $2 $3
