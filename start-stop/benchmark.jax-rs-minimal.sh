4a9461a38198805053c1e1e984f4be4bca7a641d
Galder Zamarreño <galder@redhat.com>
2022-12-21 13:38:02 +0100

    Capture 3 different RSS data points

--- start-stop/benchmark.jax-rs-minimal.sh
+++ start-stop/benchmark.jax-rs-minimal.sh
@@ -5,22 +5,31 @@ set -x
 
 startstop()
 {
-    local filename=$1
+    local dir=$1
     local version=$2
     local xmx=$3
+    local index=$4
     local app=jax-rs-minimal
+    local rssfile=$dir/memory.rss.csv
+    local logfile=$dir/out.$index.log
 
-    APP=$app QUARKUS=$version XMX=$xmx make &
+    mkdir -p $dir
 
-    sleep 4
+    APP=$app QUARKUS=$version XMX=$xmx make &> $logfile &
+
+    { tail -n +1 -f $logfile & } | sed -n '/started in/q'
 
     pid=`pidof quarkus-runner`
 
-    rss=`ps -p $pid -o rss=`
-    echo $rss >> $filename
+    psrss=`ps -p $pid -o rss=`
+    pmaprss=`pmap -X $pid | tail -1 | awk -F ' ' '{print $2}'`
 
     kill -SIGINT $pid
     sleep 2
+
+    maxrss=`grep "Maximum resident set size" $logfile | awk -F ' ' '{print $NF}'`
+
+    printf '%s,%s,%s\n' "$psrss" "$pmaprss" "$maxrss" >> $rssfile
 }
 
 trial()
@@ -28,22 +37,16 @@ trial()
     local filename=$1
     local version=$2
 
-    for xmx in \
-      64m \
-	    128m \
-	    256m \
-	    512m \
-	    1g \
-	    2g \
-	    4g
+    for i in {0..9}
     do
-        startstop $filename $version $xmx
+        startstop $filename $version 1g $i
     done
 }
 
 dir=target/benchmarks/jax-rs-minimal
 rm -drf $dir
 mkdir -p $dir
-trial $dir/2.13.4-29408-29842.rss 2.13.4-29408-29842
-trial $dir/2.7-22.3.rss 2.7-22.3
-trial $dir/2.13.rss 2.13
+trial $dir/preconfig 999
+trial $dir/2.13.4-29408-29842 2.13.4-29408-29842
+trial $dir/2.7-22.3 2.7-22.3
+trial $dir/2.13 2.13
