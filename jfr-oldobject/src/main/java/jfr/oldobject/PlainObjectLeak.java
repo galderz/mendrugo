4bc0a6e42831397c9d3e45c4518b58808033f2ca
Galder Zamarreño <galder@zamarreno.com>
2023-02-21 14:52:45 +0100

    Remove toString and hashCode implementations

--- jfr-oldobject/src/main/java/jfr/oldobject/PlainObjectLeak.java
+++ jfr-oldobject/src/main/java/jfr/oldobject/PlainObjectLeak.java
@@ -22,16 +22,6 @@ public class PlainObjectLeak
         Node left;
         Node right;
         Object value;
-
-        @Override
-        public String toString()
-        {
-            return "Node{" +
-                "left=" + left +
-                ", right=" + right +
-                ", value=" + value +
-                '}';
-        }
     }
 
     static class Big {
@@ -40,17 +30,5 @@ public class PlainObjectLeak
         float value3;
         int value4;
         double value5;
-
-        @Override
-        public String toString()
-        {
-            return "Big{" +
-                "value1=" + value1 +
-                ", value2=" + value2 +
-                ", value3=" + value3 +
-                ", value4=" + value4 +
-                ", value5=" + value5 +
-                '}';
-        }
     }
 }
