e48574a5dca21c396ec8cc358fb3868398d7ac57
Galder Zamarreño <galder@zamarreno.com>
2023-04-20 07:25:54 +0200

    Expand on getting started to see effects of static init

---
+++ quarkus-dev-crac/static-init/src/main/java/org/acme/staticinit/TimestampResource.java
@@ -0,0 +1,16 @@
+package org.acme.staticinit;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+@Path("/timestamp")
+public class TimestampResource {
+
+    static long firstAccess = System.currentTimeMillis();
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String timestamp() {
+        return "First access " + firstAccess;
+    }
+}
