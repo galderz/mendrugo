cdbba7d3547de60eb135d74634a9f529fb3f7fcb
Galder Zamarreño <galder@zamarreno.com>
2023-06-06 10:23:21 +0200

    fixup! Rename to test thread group description exclusively

--- jfr-oldobject/src/main/java/jfr/oldobject/ThreadGroupDescription.java
+++ jfr-oldobject/src/main/java/jfr/oldobject/ThreadGroupDescription.java
@@ -1,37 +1,13 @@
 package jfr.oldobject;
 
-public class ObjectDescription
+public class ThreadGroupDescription
 {
-    static Node threadGroupLeak;
-    static Node threadLeak;
+    static Node leak;
 
     public static void main(String[] args)
     {
-        threadGroupLeak = threadGroupDescription();
-        threadLeak = threadDescription();
-        Blackhole.blackhole(threadGroupLeak);
-        Blackhole.blackhole(threadLeak);
-    }
-
-    private static Node threadDescription()
-    {
-        Node node = new Node();
-        for (int i = 0; i < 1_000_000; i++)
-        {
-            node.value = new MyThread();
-            node.left = new Node();
-            node.right = new Node();
-            node = node.right;
-        }
-        return node;
-    }
-
-    public final static class MyThread extends Thread {
-        public final static String NAME = "My Thread";
-
-        public MyThread() {
-            super(NAME);
-        }
+        leak = threadGroupDescription();
+        Blackhole.blackhole(leak);
     }
 
     private static Node threadGroupDescription()
