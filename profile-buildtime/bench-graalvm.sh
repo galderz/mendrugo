bcb0056722bba26ef6d39283fd4bcce9c26d1d4f
Galder Zamarreño <galder@zamarreno.com>
2023-06-21 10:53:26 +0200

    More fixes for target directory

--- profile-buildtime/bench-graalvm.sh
+++ profile-buildtime/bench-graalvm.sh
@@ -9,31 +9,30 @@ fi
 
 bench()
 {
+    local java_dir=$1
     local java_home=$HOME/opt/$1
     local app="hibernate-orm-quickstart"
-    local target_dir="../target"
+    local target_dir="../target/$java_dir"
 
     pushd $app
 
     make clean-app
 
-    mkdir -p "$target_dir/$java_home"
-    touch "$target_dir/$java_home/console.log"
+    mkdir -p "$target_dir"
+    touch "$target_dir/console.log"
 
     make build \
         JAVA_HOME=$java_home \
         MAVEN_ARGS="-Dquarkus.native.enable-reports"
-    cp -r $app/target/$app-1.0.0-SNAPSHOT-native-image-source-jar/reports "$target_dir/$java_home"
+    cp -r $app/target/$app-1.0.0-SNAPSHOT-native-image-source-jar/reports "$target_dir"
 
     for i in `seq 1 $NUM_ITERATIONS`
     do
         rm -f $app/target/$app-1.0.0-SNAPSHOT-runner
         make build \
             JAVA_HOME=$java_home \
-            | tee -a "$target_dir/$java_home/console.log"
+            | tee -a "$target_dir/console.log"
     done
-
-    mv /tmp/times.log $target_dir/$java_home
 
     popd
 }
