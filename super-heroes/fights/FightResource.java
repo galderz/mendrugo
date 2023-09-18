7d8526217413b53f23bb01f214e8ac1444e81689
Galder Zamarreño <galder@zamarreno.com>
2023-09-18 11:41:52 +0200

    Add artificial timeout

--- super-heroes/fights/FightResource.java
+++ super-heroes/fights/FightResource.java
@@ -1,5 +1,7 @@
 package io.quarkus.workshop.superheroes.fight;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.jboss.logging.Logger;
 
 import jakarta.inject.Inject;
@@ -23,9 +25,22 @@ public class FightResource {
     @Inject
     FightService service;
 
+    @ConfigProperty(name = "process.milliseconds", defaultValue = "0")
+    long tooManyMilliseconds;
+
+    private void veryLongProcess() {
+        try {
+            Thread.sleep(tooManyMilliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
     @GET
     @Path("/randomfighters")
+    @Timeout(500) // <-- Added
     public Response getRandomFighters() {
+        veryLongProcess(); // <-- Added
         Fighters fighters = service.findRandomFighters();
         logger.debug("Get random fighters " + fighters);
         return Response.ok(fighters).build();
