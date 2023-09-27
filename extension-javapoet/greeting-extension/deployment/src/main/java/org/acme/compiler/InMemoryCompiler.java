408c5bea60bf3dd3b23ea616b83ad321bc31a4e9
Galder Zamarreño <galder@zamarreno.com>
2023-09-27 10:01:34 +0200

    Missing classpath entries when trying to compile

---
+++ extension-javapoet/greeting-extension/deployment/src/main/java/org/acme/compiler/InMemoryCompiler.java
@@ -0,0 +1,105 @@
+package org.acme.compiler;
+
+import com.squareup.javapoet.JavaFile;
+
+import javax.annotation.processing.Processor;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class InMemoryCompiler
+{
+    public static byte[] compileAsBytes(JavaFile java) {
+        final ClassLoader parent = java.getClass().getClassLoader();
+
+        final List<String> options = Collections.emptyList();
+        // final List<String> options = new ArrayList<String>();
+        // set compiler's classpath to be same as the runtime's
+        // System.out.println(System.getProperty("java.class.path"));
+        // options.addAll(Arrays.asList("-classpath", System.getProperty("java.class.path")));
+
+        final List<Processor> processors = Collections.emptyList();
+        try (InMemoryFileManager fileManager = compile(java, parent, options, processors))
+        {
+            final String name = getJavaClassName(java);
+            return fileManager.map.get(name).getBytes();
+        }
+        catch (IOException e)
+        {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    static Class<?> compileAsClass(JavaFile java) {
+        ClassLoader parent = java.getClass().getClassLoader();
+        List<String> options = Collections.emptyList();
+        List<Processor> processors = Collections.emptyList();
+        ClassLoader loader;
+        try (InMemoryFileManager fileManager = compile(java, parent, options, processors))
+        {
+            loader = fileManager.getClassLoader(null);
+            final String name = getJavaClassName(java);
+            return loader.loadClass(name);
+        }
+        catch (IOException e)
+        {
+            throw new UncheckedIOException(e);
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new IllegalStateException("loading class failed after successful compilation?!", e);
+        }
+    }
+
+    static InMemoryFileManager compile(
+        JavaFile java
+        , ClassLoader parent
+        , List<String> options
+        , List<Processor> processors
+    )
+    {
+        final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        assert compiler != null : "no system java compiler available - JDK is required!";
+        final DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
+        final StandardJavaFileManager standardFileManager = compiler.getStandardFileManager(
+            diagnostics
+            , Locale.getDefault()
+            , StandardCharsets.UTF_8
+        );
+        InMemoryFileManager manager = new InMemoryFileManager(standardFileManager, parent);
+        JavaCompiler.CompilationTask task = compiler.getTask(
+            null // a writer for additional output from the compiler; use System.err if null
+            , manager
+            , diagnostics
+            , options
+            , null // names of classes to be processed by annotation processing, null means no classes
+            , Collections.singleton(java.toJavaFileObject())
+        );
+
+        if (!processors.isEmpty())
+            task.setProcessors(processors);
+        boolean success = task.call();
+
+        if (!success)
+            throw new RuntimeException("compilation failed" + diagnostics.getDiagnostics());
+
+        return manager;
+    }
+
+    private static String getJavaClassName(JavaFile java)
+    {
+        String name = java.packageName;
+        name = name.isEmpty() ? java.typeSpec.name : name + "." + java.typeSpec.name;
+        return name;
+    }
+}
