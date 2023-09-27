0807dc1bdd62d4b559ecf57a28482e561a5ad514
Galder Zamarreño <galder@zamarreno.com>
2023-09-27 11:50:45 +0200

    Try to generate command implementation from processor

--- extension-command/command-extension/deployment/src/main/java/org/acme/command/extension/deployment/CommandExtensionProcessor.java
+++ extension-command/command-extension/deployment/src/main/java/org/acme/command/extension/deployment/CommandExtensionProcessor.java
@@ -1,7 +1,14 @@
 package org.acme.command.extension.deployment;
 
+import io.quarkus.arc.deployment.GeneratedBeanBuildItem;
+import io.quarkus.arc.deployment.GeneratedBeanGizmoAdaptor;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.gizmo.ClassCreator;
+import io.quarkus.gizmo.ClassOutput;
+import io.quarkus.gizmo.Gizmo;
+import io.quarkus.gizmo.MethodCreator;
 
 class CommandExtensionProcessor
 {
@@ -11,5 +18,25 @@ class CommandExtensionProcessor
     FeatureBuildItem feature()
     {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    void generateCommand(BuildProducer<GeneratedBeanBuildItem> generatedBeanClasses)
+    {
+        System.out.println("Generating command...");
+
+        final ClassOutput beanOutput = new GeneratedBeanGizmoAdaptor(generatedBeanClasses);
+
+        final ClassCreator command = ClassCreator.builder()
+            .classOutput(beanOutput)
+            .className("org.acme.command.generated.GeneratedCommand")
+            .interfaces("org.acme.command.main.Command") // todo share class
+            .build();
+
+        final MethodCreator run = command.getMethodCreator("run", void.class);
+        Gizmo.systemOutPrintln(run, run.load("A generated command"));
+        run.close();
+
+        command.close();
     }
 }
--- extension-command/command-main/src/main/java/org/acme/command/main/SampleCommand.java
+++ extension-command/command-main/src/main/java/org/acme/command/main/SampleCommand.java
@@ -1,13 +1,13 @@
-package org.acme.command.main;
-
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class SampleCommand implements Command
-{
-    @Override
-    public void run()
-    {
-        System.out.println("A sample command");
-    }
-}
+//package org.acme.command.main;
+//
+//import jakarta.enterprise.context.ApplicationScoped;
+//
+//@ApplicationScoped
+//public class SampleCommand implements Command
+//{
+//    @Override
+//    public void run()
+//    {
+//        System.out.println("A sample command");
+//    }
+//}
