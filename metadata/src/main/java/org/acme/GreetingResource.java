2e8cc832c2104a07dc413b569ba9589086a3614c
Galder Zamarreño <galder@zamarreno.com>
2023-10-18 14:04:08 +0200

    Involve reflection to do the greeting

--- metadata/src/main/java/org/acme/GreetingResource.java
+++ metadata/src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,24 @@
 package org.acme;
 
+import io.quarkus.logging.Log;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
-public class GreetingResource {
+public class GreetingResource
+{
+    @Inject
+    GreetingService service;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Path("/{name}")
+    public String greeting(String name)
+    {
+        Log.infof("Call greeting service with %s", name);
+        return service.greeting(name);
     }
 }
--- metadata/src/test/java/org/acme/GreetingResourceTest.java
+++ metadata/src/test/java/org/acme/GreetingResourceTest.java
@@ -7,15 +7,25 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
-
+public class GreetingResourceTest
+{
     @Test
-    public void testHelloEndpoint() {
+    public void testKnownName()
+    {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+            .when().get("/hello/Alice")
+            .then()
+            .statusCode(200)
+            .body(is("Hello Alice"));
     }
 
+    @Test
+    public void testUnknownName()
+    {
+        given()
+            .when().get("/hello/Bob")
+            .then()
+            .statusCode(404)
+            .body(is(""));
+    }
 }
