5f5874979a04081a9b3789692083b1b740d9cffa
Galder Zamarreño <galder@zamarreno.com>
2024-07-16 16:30:43 +0200

    Compare with method reflection

--- kube-introspection/src/main/java/org/acme/kube/instrospection/Introspection.java
+++ kube-introspection/src/main/java/org/acme/kube/instrospection/Introspection.java
@@ -4,6 +4,7 @@ import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 @QuarkusMain
@@ -16,6 +17,9 @@ public class Introspection implements QuarkusApplication
         final Field[] fields = clazz.getDeclaredFields();
         System.out.println("Fields size: " + fields.length);
         System.out.println("Fields: " + Arrays.toString(fields));
+        final Method[] methods = clazz.getDeclaredMethods();
+        System.out.println("Methods size: " + methods.length);
+        System.out.println("Methods: " + Arrays.toString(methods));
         return 0;
     }
 }
