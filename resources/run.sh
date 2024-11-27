658bf5584a4633f98aa789f261ef1f02a9024517
Galder Zamarreño <galder@zamarreno.com>
2024-11-27 09:52:15 +0100

    Run binary once built

--- resources/run.sh
+++ resources/run.sh
@@ -5,3 +5,4 @@ set -e
 mvn clean package
 java -jar target/resources-1.0-SNAPSHOT.jar
 native-image --no-fallback -cp target/resources-1.0-SNAPSHOT.jar org.sample.resources.ReadResource target/resources
+./target/resources
