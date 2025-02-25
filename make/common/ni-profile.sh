99e777c0937e2b37f1c9de6fa92ca1f4001f2ed7
Galder Zamarreño <galder@zamarreno.com>
2025-02-25 06:50:30 +0100

    Add native image profile target

---
+++ make/common/ni-profile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eux
+
+# e.g. mandrel-mandrel
+ID=$1
+# e.g. branches, branch-misses
+EVENT=$2
+# html or jfr
+FORMAT=$3
+
+ARGS="-J-Dlogging.initial-configurator.min-level=500 -J-Djava.util.logging.manager=org.jboss.logmanager.LogManager -J-Dsun.nio.ch.maxUpdateArraySize=100 -J-Dvertx.logger-delegate-factory-class-name=io.quarkus.vertx.core.runtime.VertxLogDelegateFactory -J-Dvertx.disableDnsResolver=true -J-Dio.netty.leakDetection.level=DISABLED -J-Dio.netty.allocator.maxOrder=3 -J-Duser.language=en -J-Duser.country=GB -J-Dfile.encoding=UTF-8 --features=io.quarkus.runner.Feature,io.quarkus.runtime.graal.DisableLoggingFeature -J--add-exports=java.security.jgss/sun.security.krb5=ALL-UNNAMED -J--add-exports=java.security.jgss/sun.security.jgss=ALL-UNNAMED -J--add-opens=java.base/java.text=ALL-UNNAMED -J--add-opens=java.base/java.io=ALL-UNNAMED -J--add-opens=java.base/java.lang.invoke=ALL-UNNAMED -J--add-opens=java.base/java.util=ALL-UNNAMED -H:+UnlockExperimentalVMOptions -H:BuildOutputJSONFile=getting-started-reactive-1.0.0-SNAPSHOT-runner-build-output-stats.json -H:-UnlockExperimentalVMOptions -H:+UnlockExperimentalVMOptions -H:+GenerateBuildArtifactsFile -H:-UnlockExperimentalVMOptions -H:+UnlockExperimentalVMOptions -H:+AllowFoldMethods -H:-UnlockExperimentalVMOptions -J-Djava.awt.headless=true --no-fallback --link-at-build-time -H:+UnlockExperimentalVMOptions -H:+ReportExceptionStackTraces -H:-UnlockExperimentalVMOptions -H:-AddAllCharsets --enable-url-protocols=http -H:NativeLinkerOption=-no-pie --enable-monitoring=heapdump -H:+UnlockExperimentalVMOptions -H:-UseServiceLoaderFeature -H:-UnlockExperimentalVMOptions -J--add-exports=org.graalvm.nativeimage/org.graalvm.nativeimage.impl=ALL-UNNAMED --exclude-config io.netty.netty-codec /META-INF/native-image/io.netty/netty-codec/generated/handlers/reflect-config.json --exclude-config io.netty.netty-handler /META-INF/native-image/io.netty/netty-handler/generated/handlers/reflect-config.json getting-started-reactive-1.0.0-SNAPSHOT-runner -jar getting-started-reactive-1.0.0-SNAPSHOT-runner.jar"
+OPT=${HOME}/opt
+
+echo "# Profile with perf stat"
+perf stat ${OPT}/${ID}/bin/native-image ${ARGS}
+
+echo "# Profile with async profiler"
+${JAVA_HOME}/bin/native-image -J-agentpath:${OPT}/async-profiler/lib/libasyncProfiler.so=start,event=${EVENT},file=${EVENT}-${ID}.${FORMAT} ${ARGS}
