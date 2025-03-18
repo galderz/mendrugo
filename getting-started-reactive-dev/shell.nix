fd9247d9410f7fdadbfe9bd6734950b3d60f92e9
Galder Zamarreño <galder@zamarreno.com>
2025-03-18 17:57:31 +0100

    Achieve GraalVM building inside nix

--- getting-started-reactive-dev/shell.nix
+++ getting-started-reactive-dev/shell.nix
@@ -1,13 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  devkit = "/nix/store/vhsix1jn849mpxggwbw2zh1nbxpy0grc-Xcode16.2-MacOSX15";
+in
 pkgs.mkShell {
   packages = [
     pkgs.gnumake
     pkgs.python3
+
+    devkit
   ];
 
   shellHook = ''
     export MX_PYTHON="${pkgs.python3}/bin/python3"
     echo "Setting MX_PYTHON to $MX_PYTHON"
+
+    export PATH=${devkit}/Xcode/Contents/Developer/usr/bin:$PATH
   '' ;
 }
