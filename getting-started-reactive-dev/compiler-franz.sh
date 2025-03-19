7bb4d8b2cbd432548c0b1d0af0d462c5e445a851
Galder Zamarreño <galder@zamarreno.com>
2025-03-19 13:31:05 +0100

    Tweak perf stat events to track

--- getting-started-reactive-dev/compiler-franz.sh
+++ getting-started-reactive-dev/compiler-franz.sh
@@ -4,10 +4,10 @@ set -eux
 
 ID=mandrel QB=3.19.3 make clean-graal clean-app
 ID=mandrel QB=3.19.3 make build
-NIR_EVENT=branch-misses:pp ID=mandrel QB=3.19.3 make ni-stat
+NIR_EVENT=branches,branch-misses,instructions,cycles,stalled-cycles-frontend ID=mandrel QB=3.19.3 make ni-stat
 NIR_EVENT=branch-misses:pp ID=mandrel QB=3.19.3 make ni-record
 
 ID=mandrel-base QB=3.19.3 make clean-graal clean-app
 ID=mandrel-base QB=3.19.3 make build
-NIR_EVENT=branch-misses:pp ID=mandrel-base QB=3.19.3 make ni-stat
+NIR_EVENT=branches,branch-misses,instructions,cycles,stalled-cycles-frontend ID=mandrel-base QB=3.19.3 make ni-stat
 NIR_EVENT=branch-misses:pp ID=mandrel-base QB=3.19.3 make ni-record
