4c1d45b3f21b25d351e5ab215ceeff75a4dad964
Galder Zamarreño <galder@zamarreno.com>
2025-03-19 17:01:34 +0100

    Use separate directories to keep both

--- getting-started-reactive-dev/compiler-franz.sh
+++ getting-started-reactive-dev/compiler-franz.sh
@@ -2,12 +2,12 @@
 
 set -eux
 
-ID=mandrel QB=3.19.3 make clean-graal clean-app
-ID=mandrel QB=3.19.3 make build
-NIR_EVENT=branches,branch-misses,instructions,cycles,stalled-cycles-frontend ID=mandrel QB=3.19.3 make ni-stat
-NIR_EVENT=branch-misses:pp ID=mandrel QB=3.19.3 make ni-record
+ID=mandrel QB=3.19.3 APP_DIR=compiler-patch make clean-graal clean-app
+ID=mandrel QB=3.19.3 APP_DIR=compiler-patch make build
+NIR_EVENT=branches,branch-misses,instructions,cycles,stalled-cycles-frontend ID=mandrel QB=3.19.3 APP_DIR=compiler-patch make ni-stat
+NIR_EVENT=branch-misses:pp ID=mandrel QB=3.19.3 APP_DIR=compiler-patch make ni-record
 
-ID=mandrel-base QB=3.19.3 make clean-graal clean-app
-ID=mandrel-base QB=3.19.3 make build
-NIR_EVENT=branches,branch-misses,instructions,cycles,stalled-cycles-frontend ID=mandrel-base QB=3.19.3 make ni-stat
-NIR_EVENT=branch-misses:pp ID=mandrel-base QB=3.19.3 make ni-record
+ID=mandrel-base QB=3.19.3 APP_DIR=compiler-base make clean-graal clean-app
+ID=mandrel-base QB=3.19.3 APP_DIR=compiler-base make build
+NIR_EVENT=branches,branch-misses,instructions,cycles,stalled-cycles-frontend ID=mandrel-base QB=3.19.3 APP_DIR=compiler-base make ni-stat
+NIR_EVENT=branch-misses:pp ID=mandrel-base QB=3.19.3 APP_DIR=compiler-base make ni-record
