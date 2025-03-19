e4c33946c67ea5a3f75f6fd783c9b95abfcd8d25
Galder Zamarreño <galder@zamarreno.com>
2025-03-19 13:23:08 +0100

    Do not start Quarkus but only build it

--- getting-started-reactive-dev/compiler-franz.sh
+++ getting-started-reactive-dev/compiler-franz.sh
@@ -3,11 +3,11 @@
 set -eux
 
 ID=mandrel QB=3.19.3 make clean-graal clean-app
-ID=mandrel QB=3.19.3 make
+ID=mandrel QB=3.19.3 make build
 NIR_EVENT=branch-misses:pp ID=mandrel QB=3.19.3 make ni-stat
 NIR_EVENT=branch-misses:pp ID=mandrel QB=3.19.3 make ni-record
 
 ID=mandrel-base QB=3.19.3 make clean-graal clean-app
-ID=mandrel-base QB=3.19.3 make
+ID=mandrel-base QB=3.19.3 make build
 NIR_EVENT=branch-misses:pp ID=mandrel-base QB=3.19.3 make ni-stat
 NIR_EVENT=branch-misses:pp ID=mandrel-base QB=3.19.3 make ni-record
