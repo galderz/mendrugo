5cf8d5617f32a18359b0a1315e9a507b7e1226c0
Galder Zamarreño <galder@zamarreno.com>
2025-06-03 10:35:37 +0200

    Trace inlining with single threaded build and log output

---
+++ more-inlining/trace-inlining.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+APP_DIR=quarkus-reactive-beer
+CE_HOME=$HOME/opt/graal-24
+EE_HOME=$HOME/opt/ee-graal-24
+
+build()
+{
+    local extra_args=$1
+    local graalvm_home=$2
+    local log_id=$3
+    local common_args="-H:+TraceInlining,-H:NumberOfThreads=1"
+
+    make clean
+    GRAALVM_HOME=${graalvm_home} NATIVE_BUILD_ARGS="${common_args},${extra_args}" make build 2>&1 | tee ${log_id}
+}
+
+# Round 7
+build "-H:-ClampMLInferredProfiles,-H:-MLProfileInference,-H:-MLProfileInferenceGuards" ${EE_HOME} "ee-disable-ml.trace-inlining"
+build "" ${EE_HOME} "ee-base.trace-inlining"
+build "" ${CE_HOME} "ce.trace-inlining"
