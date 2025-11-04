9c9dad0a47aa7dcf3aa3a455b0dff573efa4b7a8
Galder Zamarreño <galder@ibm.com>
2025-11-04 11:43:04 +0100

    Copy from startupperf-hello

---
+++ future-defaults/startupperf-security.java
@@ -0,0 +1,119 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS org.json:json:20250517
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
+
+public class startup {
+
+    private static final long TIMEOUT_NS = Duration.ofSeconds(3).toNanos();
+
+    public static void main(String... args) throws Exception {
+        final TreeMap<String, List<String>> cmds = new TreeMap<>();
+        cmds.put("hello", List.of("./quickstart/target/getting-started-1.0.0-SNAPSHOT-runner"));
+        cmds.forEach((k, v) -> {
+            System.out.println("===========================\n" + k + "\n" + v);
+            try {
+                for (int i = 0; i < 10; i++) {
+                    runProcess(v, true);
+                }
+                runProcess(v, false);
+            } catch (Exception e) {
+                System.err.println("Eh..: " + e.getMessage());
+            }
+        });
+    }
+
+    private static void runProcess(List<String> command, boolean silent)
+        throws URISyntaxException, IOException, InterruptedException {
+        final HttpClient client = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofMillis(25)) // eh, sketchy
+            .build();
+        final HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI("http://localhost:8080/hello"))
+            .GET()
+            .build();
+        final Path logFile = Files.createTempFile("quarkus-run-", ".log");
+        Process process = null;
+        try {
+            process = new ProcessBuilder(command)
+                .redirectErrorStream(true)
+                .redirectOutput(logFile.toFile())
+                .start();
+            long startTimeNs = System.nanoTime();
+            if (!silent) {
+                System.out.println("Quarkus process started (PID: " + process.pid() + "). Polling for first response...");
+            }
+            long firstResponseTimeNs = 0;
+            while (System.nanoTime() - startTimeNs < TIMEOUT_NS) {
+                try {
+                    final HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
+                    if (response.statusCode() == 200 && response.body().length > 0) {
+                        firstResponseTimeNs = System.nanoTime() - startTimeNs;
+                        if (!silent) {
+                            System.out.printf("First HTTP 200 response after %.6f ms%n", firstResponseTimeNs / 1_000_000.0);
+                        }
+                        break;
+                    }
+                } catch (java.net.http.HttpConnectTimeoutException | java.net.ConnectException | InterruptedException e) {
+                    // nop
+                }
+                Thread.yield();
+            }
+            if (firstResponseTimeNs == 0) {
+                System.err.println("Meh, giving up.");
+            }
+            if (!silent) {
+                System.out.println("RSS: " + getRSSkB(process.pid()) / 1024 + " MB");
+            }
+        } finally {
+            if (process != null) {
+                process.destroy();
+                process.waitFor();
+            }
+            if (logFile != null && Files.exists(logFile)) {
+                if (!silent) {
+                    Files.readAllLines(logFile).stream()
+                        .filter(l -> l.contains("started in")).findFirst()
+                        .ifPresent(System.out::println);
+                }
+                Files.delete(logFile);
+            }
+        }
+    }
+
+    private static final Pattern NUM_PATTERN = Pattern.compile("[ \t]*[0-9]+[ \t]*");
+
+    public static long getRSSkB(long pid) throws IOException, InterruptedException {
+        ProcessBuilder pa = new ProcessBuilder("ps", "-p", Long.toString(pid), "-o", "rss=");
+        final Map<String, String> envA = pa.environment();
+        envA.put("PATH", System.getenv("PATH"));
+        pa.redirectErrorStream(true);
+        final Process p = pa.start();
+        try (BufferedReader processOutputReader =
+                 new BufferedReader(new InputStreamReader(p.getInputStream(), StandardCharsets.UTF_8))) {
+            String l;
+            while ((l = processOutputReader.readLine()) != null) {
+                if (NUM_PATTERN.matcher(l).matches()) {
+                    return Long.parseLong(l.trim());
+                }
+            }
+            p.waitFor();
+        }
+        return -1L;
+    }
+}
