36f873e4c537345e791b4b9a90555aa9bace2d95
Galder Zamarreño <galder@zamarreno.com>
2025-12-19 11:30:19 +0100

    Build a base layer with a dummy module

---
+++ layers/build-base.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eux
+
+mkdir -p target
+
+pushd dummy-module
+make
+popd
+
+native-image -H:LayerCreate==base-layer.nil,module=java.base,module=dummy.module --module-path dummy-module/target/dummy.module.jar -o target/libjavabase
