3261d99cd69a3842c13876bf7684e9318c11bc50
Galder Zamarreño <galder@zamarreno.com>
2026-01-22 09:04:32 +0100

    Forced init ExtendedSocketOptions at runtime

--- layers/build-base.sh
+++ layers/build-base.sh
@@ -7,6 +7,11 @@ pushd dummy-module
 make
 popd
 
+#    --initialize-at-run-time=sun.net.ext.ExtendedSocketOptions,com.sun.media.sound.PortMixerProvider \
+#    --debug-attach=*:8000 \
+#    -H:ApplicationLayerOnlySingletons=org.graalvm.nativeimage.impl.RuntimeSystemPropertiesSupport \
+
 native-image \
     -H:+PrintClassInitialization \
+    --initialize-at-run-time=sun.net.ext.ExtendedSocketOptions \
     -H:LayerCreate=base-layer.nil,module=java.base,module=dummy.module --module-path dummy-module/target/dummy.module.jar -o target/libjavabase
