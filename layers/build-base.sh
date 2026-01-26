c35107a70ef7f583fa2d796fd1db63aac3e253f9
Galder Zamarreño <galder@zamarreno.com>
2026-01-26 09:02:04 +0100

    Back to having base layer runtime init, plus changes in app layer

--- layers/build-base.sh
+++ layers/build-base.sh
@@ -3,9 +3,9 @@ set -eux
 
 mkdir -p target
 
-pushd dummy-module
-make
-popd
+#pushd dummy-module
+#make
+#popd
 
 #    --initialize-at-run-time=sun.net.ext.ExtendedSocketOptions,com.sun.media.sound.PortMixerProvider \
 #    --initialize-at-run-time=sun.net.ext.ExtendedSocketOptions \
@@ -17,19 +17,41 @@ popd
 # --trace-object-instantiation=java.rmi.server.ObjID \
 #--initialize-at-build-time=jdk.internal.jimage \
 
+#--initialize-at-build-time= \
+#                          --initialize-at-build-time=jdk.internal.jimage.ImageLocation \
+#                          --initialize-at-build-time=jdk.internal.jimage.ImageReader \
+#                          --initialize-at-build-time=jdk.internal.jimage.ImageReader\$Directory \
+#                          --initialize-at-build-time=jdk.internal.jimage.ImageReader\$SharedImageReader \
+#                          --initialize-at-build-time=jdk.internal.jimage.ImageReader\$Resource \
+#                          --initialize-at-run-time=java.rmi.server.ObjID \
+#                          --initialize-at-run-time=jdk.jpackage.internal.LinuxPackageArch\$DebPackageArch \
+#                          --initialize-at-run-time=sun.awt.dnd \
+#                          --initialize-at-run-time=sun.awt.X11 \
+#                          --initialize-at-run-time=sun.awt.X11GraphicsConfig \
+#                          --initialize-at-run-time=sun.rmi.registry \
+#                          --initialize-at-run-time=sun.rmi.transport.DGCClient \
+
+#native-image \
+#    -H:+PrintClassInitialization \
+#    -H:LayerCreate=base-layer.nil,module=java.base,module=dummy.module --module-path dummy-module/target/dummy.module.jar -o target/libjavabase
+
+#native-image \
+#    --initialize-at-build-time= \
+#    --initialize-at-run-time=java.rmi.server.ObjID \
+#    --initialize-at-run-time=jdk.jpackage.internal.LinuxPackageArch\$DebPackageArch \
+#    --initialize-at-run-time=sun.awt.dnd \
+#    --initialize-at-run-time=sun.awt.X11 \
+#    --initialize-at-run-time=sun.awt.X11GraphicsConfig \
+#    --initialize-at-run-time=sun.rmi.registry \
+#    --initialize-at-run-time=sun.rmi.transport \
+#    --initialize-at-build-time=jdk.internal.jimage.ImageLocation \
+#    --initialize-at-build-time=jdk.internal.jimage.ImageReader \
+#    --initialize-at-build-time=jdk.internal.jimage.ImageReader\$Directory \
+#    --initialize-at-build-time=jdk.internal.jimage.ImageReader\$Resource \
+#    --initialize-at-build-time=jdk.internal.jimage.ImageReader\$SharedImageReader \
+#    -H:LayerCreate=libjavabaselayer.nil,module=java.base -o libjavabaselayer -H:Path=./target
+
 native-image \
     -H:+PrintClassInitialization \
-    --initialize-at-build-time= \
-    --initialize-at-build-time=jdk.internal.jimage.ImageLocation \
-    --initialize-at-build-time=jdk.internal.jimage.ImageReader \
-    --initialize-at-build-time=jdk.internal.jimage.ImageReader\$Directory \
-    --initialize-at-build-time=jdk.internal.jimage.ImageReader\$SharedImageReader \
-    --initialize-at-build-time=jdk.internal.jimage.ImageReader\$Resource \
-    --initialize-at-run-time=java.rmi.server.ObjID \
-    --initialize-at-run-time=jdk.jpackage.internal.LinuxPackageArch\$DebPackageArch \
-    --initialize-at-run-time=sun.awt.dnd \
-    --initialize-at-run-time=sun.awt.X11 \
-    --initialize-at-run-time=sun.awt.X11GraphicsConfig \
-    --initialize-at-run-time=sun.rmi.registry \
-    --initialize-at-run-time=sun.rmi.transport.DGCClient \
-    -H:LayerCreate=base-layer.nil,module=java.base,module=dummy.module --module-path dummy-module/target/dummy.module.jar -o target/libjavabase
+    -H:LayerCreate=libjavabaselayer.nil,module=java.base -o libjavabaselayer -H:Path=./target
+
