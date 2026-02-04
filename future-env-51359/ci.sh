4c8be34cbf10f999b3dfa195079a799413c28c60
Galder Zamarreño <galder@ibm.com>
2026-02-04 09:37:24 +0100

    Avoid checking toolchain

--- future-env-51359/ci.sh
+++ future-env-51359/ci.sh
@@ -3,4 +3,4 @@
 set -e -x
 
 #QUARKUS_NATIVE_ADDITIONAL_BUILD_ARGS_APPEND=--future-defaults=all TEST_MODULE=main make test-quarkus
-NATIVE_IMAGE_OPTIONS=--future-defaults=all TEST_MODULE=main make test-quarkus
+NATIVE_IMAGE_OPTIONS="--future-defaults=all -H:-CheckToolchain" TEST_MODULE=main make test-quarkus
